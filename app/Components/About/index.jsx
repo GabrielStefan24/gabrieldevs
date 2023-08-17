33f257cdf54d4b75291d453ee06c18c3f37e3939
Sirbu Gabriel <sirbugabriel97@gmail.com>
2023-08-17 19:59:57 +0300

    modified hero component

--- app/Components/About/index.jsx
+++ app/Components/About/index.jsx
@@ -1,25 +1,43 @@
 import styles from "./styles.module.scss";
+import { motion, useInView } from "framer-motion";
+import { slideUp, opacity } from "./variants";
+import { useRef } from "react";
 
 const About = () => {
+  const container = useRef(null);
+  const isInView = useInView(container);
+  console.log(isInView);
+
   const phrase =
     "Hi, I'm Gabriel, a self-taught front-end web developer. Passionate about learning new technologies and pushing boundaries, I blend design and development to create optimal user experiences. ";
 
   return (
-    <div className={styles.description}>
+    <div className={styles.description} ref={container}>
       <div className={styles.container}>
         <p>
           {phrase.split(" ").map((word, index) => {
             return (
-              <span className={styles.mask}>
-                <span>{word}</span>
+              <span className={styles.mask} key={index}>
+                <motion.span
+                  custom={index}
+                  variants={slideUp}
+                  initial="initial"
+                  animate={isInView ? "open" : " closed"}
+                >
+                  {word}
+                </motion.span>
               </span>
             );
           })}
         </p>
-        <p>
+        <motion.p
+          variants={opacity}
+          initial="initial"
+          animate={isInView ? "open" : " closed"}
+        >
           My passion for design and coding fuels my love for web interaction. I
           thoroughly enjoy both the design and coding aspects of web development
-        </p>
+        </motion.p>
       </div>
     </div>
   );
---
+++ app/Components/About/variants.js
@@ -0,0 +1,27 @@
+import { delay } from "framer-motion";
+
+export const opacity = {
+  initial: {
+    opacity: 0,
+  },
+
+  open: {
+    opacity: 1,
+    transition: { duration: 1.2 },
+  },
+  closed: {
+    opacity: 0,
+  },
+};
+export const slideUp = {
+  initial: {
+    y: "100%",
+  },
+  open: (i) => ({
+    y: 0,
+    transition: { duration: 0.5, delay: i * 0.02 },
+  }),
+  closed: {
+    y: "100%",
+  },
+};
